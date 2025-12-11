13a501f9640fe6206922182a9ece360ec9ab81d4
Diogo Santos <diogomesquitas59@gmail.com>
2025-12-11 23:55:55 +0000

    fix: build issues, upgrade next

--- components/layout/client-providers.tsx
+++ components/layout/client-providers.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import { UserPreferencesProvider } from '@/lib/context/UserPreferencesContext'
+import { BusinessProvider } from '@/lib/business-context'
 import { ReactNode } from 'react'
 
 export function ClientProviders({ children }: { children: ReactNode }) {
   return (
     <UserPreferencesProvider>
-      {children}
+      <BusinessProvider>
+        {children}
+      </BusinessProvider>
     </UserPreferencesProvider>
   )
 }

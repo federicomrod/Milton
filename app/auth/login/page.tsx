1f05b92a2297f3585d31f09dadf3d58f067535d8
Diogo Santos <diogomesquitas59@gmail.com>
2026-03-03 20:01:15 +0000

    fix: build issue

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 import { LoginForm } from "@/components/auth/login-form";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export default function LoginPage() {
   return (
